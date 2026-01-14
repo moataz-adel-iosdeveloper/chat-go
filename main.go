3eed457393a82cdbd70d191785cf2caa7e406f9a
Moataz Adel <moataz_adel2010@yahoo.com>
2026-01-14 16:07:32 +0200

    update main app

--- main.go
+++ main.go
@@ -1,46 +1,51 @@
 package main
 
 import (
+	"chat-go/cmd/db"
+	"chat-go/cmd/server"
+	"chat-go/internal/services"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	// Load .env only in local development
-	// if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
-	// 	_ = godotenv.Load()
-	// }
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Println("No .env file found")
-	// 	log.Fatal(err)
-	// }
-	// services.InitJWTService()
-	// client, err := db.InitDB()
-	// if err != nil {
-	// 	log.Fatal("Failed to initialize database:", err)
-	// }
-	// // initialize services with mongo client before starting the HTTP server
-	// services.InitDB(client)
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("No .env file found")
+			log.Fatal(err)
+		}
+	}
+	services.InitJWTService()
+	client, err := db.InitDB()
+	if err != nil {
+		log.Fatal("Failed to initialize database:", err)
+	}
+	// initialize services with mongo client before starting the HTTP server
+	services.InitDB(client)
 
-	// // pass mongo client to server and start it
-	// newServer := server.NewWebServer(client)
-	// if err := newServer.Connect(); err != nil {
-	// 	log.Fatal(err)
-	// }
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	// pass mongo client to server and start it
+	newServer := server.NewWebServer(client)
+	if err := newServer.Connect(); err != nil {
+		log.Fatal(err)
 	}
 
-	mux := http.NewServeMux()
+	// test server
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// }
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Railway OK test"))
-	})
+	// mux := http.NewServeMux()
 
-	log.Println("Listening on :" + port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.WriteHeader(http.StatusOK)
+	// 	w.Write([]byte("Railway OK test"))
+	// })
+
+	// log.Println("Listening on :" + port)
+	// log.Fatal(http.ListenAndServe(":"+port, mux))
 }
