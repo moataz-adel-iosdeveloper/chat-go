b5284d87273c2f574ef294473671618d4af97b43
Moataz Adel <moataz_adel2010@yahoo.com>
2026-01-14 14:59:25 +0200

    update to make minimal test

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,38 @@
 package main
 
 import (
-	"chat-go/cmd/db"
-	"chat-go/cmd/server"
-	"chat-go/internal/services"
 	"log"
-
-	"github.com/joho/godotenv"
+	"net/http"
+	"os"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found")
-		log.Fatal(err)
-	}
-	services.InitJWTService()
-	client, err := db.InitDB()
-	if err != nil {
-		log.Fatal("Failed to initialize database:", err)
-	}
-	// initialize services with mongo client before starting the HTTP server
-	services.InitDB(client)
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Println("No .env file found")
+	// 	log.Fatal(err)
+	// }
+	// services.InitJWTService()
+	// client, err := db.InitDB()
+	// if err != nil {
+	// 	log.Fatal("Failed to initialize database:", err)
+	// }
+	// // initialize services with mongo client before starting the HTTP server
+	// services.InitDB(client)
 
-	// pass mongo client to server and start it
-	newServer := server.NewWebServer(client)
-	if err := newServer.Connect(); err != nil {
-		log.Fatal(err)
+	// // pass mongo client to server and start it
+	// newServer := server.NewWebServer(client)
+	// if err := newServer.Connect(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Railway OK"))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
